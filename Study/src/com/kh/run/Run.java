962210bce48cc6a5b9e605965c41c2fdf6c11b81
yunseokhwa <yunseog56@gmail.com>
2024-03-14 19:31:06 +0900

    1주차 2째날

--- Study/src/com/kh/run/Run.java
+++ Study/src/com/kh/run/Run.java
@@ -2,6 +2,8 @@ package com.kh.run;
 
 import com.kh.variable.A_Variable;
 import com.kh.variable.B_keyboardlnput;
+import com.kh.variable.C_Printf;
+import com.kh.variable.D_Cast;
 
 public class Run {
 
@@ -13,7 +15,16 @@ public class Run {
 		 
 	   B_keyboardlnput b = new B_keyboardlnput();
 	  // b.inputTest4();
-	  b.charAtTest();
+	 // b.charAtTest();
+	   
+	   C_Printf c = new C_Printf();
+	   c.printfTest();
+	   
+	   D_Cast d = new D_Cast();
+	   d.autoCasting();
+	   d.autoCasting();
+	   
+	   
 
 	}
 
