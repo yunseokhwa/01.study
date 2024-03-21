61c1d13ca145d974dc8bf9b0b242955ded067144
yunseokhwa <151510163+yunseokhwa@users.noreply.github.com>
2024-03-22 00:11:35 +0900

    04_Array

--- 04_Array/src/com/kr/array/B_ArrayCopy.java
+++ 04_Array/src/com/kr/array/B_ArrayCopy.java
@@ -1,97 +1,115 @@
 package com.kr.array;
 
+import java.util.Arrays;
+//import java .lang*;
 public class B_ArrayCopy {
-	//배열복사 
+	//배열복사
 	public void method1() {
-	int[]origin = {1,2,3,4,5};
-	
-	System.out.println("===원본 배열 출력===");
-	for(int i=0; i<origin.length;i++) {
-		System.out.print(origin[i]+"");
-	}
-	//단순하게 origin 을 다시 대입시킨 copy 배열 셋팅
-	
-	int[] copy = origin;
-	System.out.println("=== 복사본 배열 출력 ===");
-	for(int i=0; i<copy.length; i++) {
-		System.out.print(copy[i]+ "");
-	}
-	
-	System.out.println();
-	copy[2]=99;
-	System.out.println("----------복사본 배열 값 변경 후---------");
-	
-     System.out.println("===원본 배열 출력 ======");
-	 for(int i=0; i<origin.length;i++) {
-		 System.out.print(origin[i]+"");
-	 }
-	 System.out.println();
-	 
-	 //단순하게 origin 을 다시 대입시킨 copy 배열 셋팅
-	 
-	 System.out.println("===복사본 배열 출력 ===");
-	 for(int i=0; i<copy.length; i++) {
-		 System.out.print(copy[i]+"");
-	 }
-	 System.out.println();
-	 //copy 만을 가지고 수정을 해도 원본까지 변경되어 있음
-	 //왜? origin 과 copy 과 같은 곳을 참조하고 있기 때문 (즉, 같은 주소값)
-	System.out.println("origin 주소값 :" +origin.hashCode());
-	System.out.println("copy 의 주소값 :" +copy.hashCode());
-	// 얇은 복사 : 주소값을 복사 
-	 
-	}
-	// 얇은 복사의 문제를 해결하기 위해서 깊은 복사 
-	public void method2() {
-		//1.for문 활용한 방법 
-		// 새로운 배열을 아사리 생성한후 반복을 활용해서 
-		//원본배열의 값들을 새로이 만든 배열에 대입하는 방법
-		int[]origin = {1,2,3,4,5};
+		int[]origin= {1,2,3,4,5};
 		
-		// int[] copy =origein;//얇은 복사 :주소값 대입
-		int[] copy =new int[5];// 새로운 주소값
-		/*
-		copy [0]= origin[0];
-		copy [1]= origin[1];
-		copy [2]= origin[2];
-		copy [3]= origin[3];
-		copy [4]= origin[4];
-		*/
-		for(int i=0; i<copy.length; i++) {
-			copy[i]=origin[i];
-		}
-		//copy 배열 수정해보기
-	     copy[2]=99;
-	     
-	     System.out.println("-----복사본 배열 값 변경 후 -------");
-	     
-	      System.out.println("====원본 배열 출력 ====");
-	      for(int i =0; i<origin.length;i++) {
-	    	  System.out.print(origin[i]+"");
-	    	  
+		System.out.println("===원본 배열 출력===");
+		for(int i=0; i<origin.length;i++) {
+			System.out.print(origin[i]+"");
+	
+	}
+		   // 단순하게 origin을 다시 대입시킨 copy배열 셋팅
+	      
+	      int[] copy = origin;
+	      System.out.println("=== 복사본 배열 출력 ===");
+	      for(int i=0; i<copy.length; i++) {
+	         System.out.print(copy[i] + " ");
 	      }
+	      
 	      System.out.println();
 	      
-	    //단순하게 origin을 다시 대입시킨 copy배열 셋팅 
+	      copy[2] = 99;
 	      
-	      System.out.println("===복사본 배열 출력 ===");
+	      System.out.println("-------복사본 배열 값 변경 후------- ");
+	      
+	      System.out.println("=== 원본 배열 출력 ===");
+	      for(int i = 0; i<origin.length;i++) {
+	         System.out.print(origin[i]+ " ");
+	      }
+	      
+	      System.out.println();
+	      
+	      
+	      // 단순하게 origin을 다시 대입시킨 copy배열 셋팅
+	      
+	      
+	      System.out.println("=== 복사본 배열 출력 ===");
 	      for(int i=0; i<copy.length; i++) {
-	    	  System.out.println(copy[i]+"");
+	         System.out.print(copy[i] + " ");
 	      }
 	      
 	      System.out.println();
 	      //copy 만을 가지고 수정을 해도 원본까지 변경되어 있음
-	      //왜? origin 과 copy가 같은 곳을 참조하고 있기 때문(즉, 같은 주소값)
-	      System.out.println("origin주소값 :" +origin.hashCode());
-	      System.out.println("copy의 주소값 :"+copy.hashCode());
+	      //왜? origin 과copy가 같은 곳을 참조하고 있기 때문 ( 즉, 같은 주소값)
+	     System.out.println("origin 주소값 : "+origin.hashCode() );
+	     System.out.println("copy 의 주소값 :"+ copy.hashCode());
+	     
+	     //얇은 복사 : 주소값을 복사
+	     
 	      
-	      //얇은 복사 : 주소값을 복사
-	}
-	public void method3() {
-		//2.아싸리 새로운배열 생성후 system이라는 클래스애서 arratcopy()메소드 를 이용한 복사
-		int[] origin = {1,2,3,4,5};
-		int[] copy =new int[10]; //넉넉 하게 크게 생성 /0번 ~9번 인덱스까지 0이 담겨있음 
-		 
+	   }
+	
+	     //얇은 복사의 문제를 해결하기 위해서 깊은 복사 
+        public void method2() {
+        	//1.for문을 활용한 방법
+        	//새로운 배열을 아싸리 생성한후 반복을 활용해서 
+        	//원본배열의 값들을 새로이 만든 배열에 대입하는 방법
+        	int[]origin = {1,2,3,4,5};
+        	
+        	//int[] copy = origein;// 얇은 복사 :주소값 대입
+        	int[]copy =new int[5];//새로운 주소값
+        	/*
+        	copy [0]= origin[0];
+        	copy [1]= origin[1];
+        	copy [2]= origin[2];
+        	copy [3]= origin[3];
+        	copy [4]= origin[4];
+        	*/
+        	for(int i=0; i<copy.length; i++) {
+        		copy[i]=origin[i];
+        		
+        	}
+        	//copy 배열 수정해보기
+        	copy[2]=99;
+        	
+        	
+        	   System.out.println("-------복사본 배열 값 변경 후------- ");
+     	      
+     	      System.out.println("=== 원본 배열 출력 ===");
+     	      for(int i = 0; i<origin.length;i++) {
+     	         System.out.print(origin[i]+ " ");
+     	      }
+     	      
+     	      System.out.println();
+     	      
+     	      
+     	      // 단순하게 origin을 다시 대입시킨 copy배열 셋팅
+     	      
+     	      
+     	      System.out.println("=== 복사본 배열 출력 ===");
+     	      for(int i=0; i<copy.length; i++) {
+     	         System.out.print(copy[i] + " ");
+     	      }
+     	      
+     	      System.out.println();
+     	      //copy 만을 가지고 수정을 해도 원본까지 변경되어 있음
+     	      //왜? origin 과copy가 같은 곳을 참조하고 있기 때문 ( 즉, 같은 주소값)
+     	     System.out.println("origin 주소값 : "+origin.hashCode() );
+     	     System.out.println("copy 의 주소값 :"+ copy.hashCode());
+     	     
+     	     //얇은 복사 : 주소값을 복사
+     	     
+     	      
+     	   }
+        public void method3() {
+        	//2.아싸리 새로운배열 생성후 System 이라는 클래스에서 arratcopy() 메소드를 이용한 복사 
+        int[] origin = {1,2,3,4,5};
+        int[] copy = new int[10]; //넉넉 하게 크게 생성 /0번~9번 인덱스까지 0이 다 담겨있을 것!!
+        
         // System.arraycopy(원본배열명, 복사시작할인덱스,복사본배열명,복사본 배열의 복사될 시작인덱스, 복사할개수 );
          //System.arraycopy(origin,0,copy,0,5);
          //System.arraycopy(origin,0,copy,2,5);
@@ -178,4 +196,3 @@ public class B_ArrayCopy {
 	
 	  
 	
-}
--- 04_Array_pr/src/com/kr/practice/array/ArrayPractice.java
+++ 04_Array_pr/src/com/kr/practice/array/ArrayPractice.java
@@ -1,9 +1,11 @@
 package com.kr.practice.array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+import javax.lang.model.util.Elements.Origin;
+
 public class ArrayPractice {
-	
 	public void practice1() {
 
 		int[] arr = new int[10];
@@ -36,4 +38,352 @@ public class ArrayPractice {
 			System.out.print((arr[i] + " "));
 
 		}
+	}
+
+	public void practice4() {
+
+		String[] arr = { "사과", "귤", "포도", "복숭아", "참외" };
+		System.out.println(arr[1]);
+
+	}
+
+	public void practice5() {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("문자열 : ");
+		String str = sc.nextLine();
+
+		char[] arr = new char[str.length()];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = str.charAt(i);
+		}
+
+		System.out.print("문자 : ");
+		char ch = sc.nextLine().charAt(0);
+
+		int count = 0;
+
+		System.out.print(str + "에" + ch + " 가 존재하는 위치(인덱스) : ");
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == ch) {
+				System.out.print(i + " ");
+				count++;
+			}
+
+		}
+
+		System.out.println();
+		System.out.println(ch + " 개수 : " + count);
+
+	}
+
+	public void practice6() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("0 ~ 6 사이 숫자 입력 : ");
+		int num = sc.nextInt();
+
+		char[] week = { '월', '화', '수', '목', '금', '토', '일' };
+
+		if (num >= 0 && num < 7) {
+			System.out.println(week[num] + "요일");
+		} else {
+			System.out.println("잘못 입력하셨습니다.");
+		}
+
+	}
+
+	public void practice7() {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("정수를 입력하세요: ");
+		int n = scanner.nextInt();
+
+		int[] arr = new int[n];
+		int sum = 0;
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("배열 " + i + "번째 인덱스에 넣을 값: ");
+			arr[i] = scanner.nextInt();
+			sum += arr[i];
+		}
+
+		System.out.print("배열의 값: ");
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+		System.out.println("\n총 합: " + sum);
+
+	}
+
+	public void practice8() {
+
+		Scanner sc = new Scanner(System.in);
+		int n;
+
+		while (true) {
+			System.out.print("정수: ");
+			n = sc.nextInt();
+			if (n % 2 == 1 && n >= 3) {
+				break;
+			} else {
+				System.out.println("다시 입력하세요.");
+			}
+		}
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n / 2; i++) {
+			arr[i] = i + 1;
+		}
+		for (int i = n / 2; i < n; i++) {
+			arr[i] = n / 2 + 1 - (i - n / 2);
+		}
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+	public void practice9() {
+		
+		
+		/*
+		 * 사용자가 입력한 값이 배열에 있는지 검색하여
+           있으면 “OOO 치킨 배달 가능“, 없으면 “OOO 치킨은 없는 메뉴입니다“를 출력하세요.
+            단, 치킨 메뉴가 들어가있는 배열은 본인 스스로 정하세요.
+		 * 
+		 * 
+		 * 
+		 * 
+		 */Scanner sc = new Scanner(System.in);
+	      // 배열 생성 
+	      String[] chickens = {"황금올리브","허니콤보","뿌링클"};
+	      
+	      //int count = 0;
+	      boolean flag = false; // 현재 플래그라는 박스에 펄스가 있음 (치킨이 없는 것 )
+	      
+	      System.out.print("치킨 이름을 입력하세요 : " );
+	      String chicken = sc.nextLine();
+	      
+	      for(int i=0; i<chickens.length; i++) {
+	         if(chickens[i].equals(chicken)) {
+	            //count++;
+	            flag = true; //( 치킨이 있는 것 )
+	         }
+
+	      }
+	      
+	      if(flag == true) { //치킨이 있는경우  // if(count == 1) 로 풀수도 있음 
+	         System.out.println(chicken +"치킨 배달 가능");
+	      }else { // 치킨이 없는 경우 
+	         System.out.println(chicken + "치킨 배달 불가");
+	      }
+	   }
+		
+	public void practice10() {
+		/*
+		 * 주민등록번호 성별자리 이후부터 *로 가리고 출력하세요. 단, 원본 배열 값은 변경 없이 배열 복사본으로 변경하세요.
+		 */
+		Scanner sc = new Scanner(System.in);
+		System.out.print("주민등록번호(-포함) : ");
+		String str = sc.nextLine();
+
+		char[] arr = new char[14];
+		for (int i = 0; i < arr.length; i++) {
+
+			if (i > 7) {
+				arr[i] = '*';
+			} else {
+				arr[i] = str.charAt(i);
+			}
+			System.out.print(arr[i]);
+		}
+
+	}
+
+	public void practice11() {
+		int[] arr = new int[10];
+	    for(int i = 0; i < arr.length; i++) {
+	        int random = (int)(Math.random()* 10 +1);
+	        arr[i] = random;
+	    }
+	    for(int i = 0; i < arr.length; i++) {
+	        System.out.print(arr[i] + " ");
+	    }
+	}
+
+	public void practice12() {
+		 int[] arr = new int[10];
+
+		    for(int i = 0; i < arr.length; i++) {
+		        int random = (int)(Math.random() * 10 + 1);
+		        arr[i] = random;
+		    }
+		    int max = arr[0];
+		    int min = arr[0];
+
+		    for(int i = 0; i < arr.length-1; i++) {
+		        if(arr[i] >= max) {
+		            max = arr[i]; 
+		        }
+		        if(arr[i] <= min) {
+		            min = arr[i];
+		        }
+		    }
+		    for(int i = 0; i < arr.length; i++) {
+		        System.out.print(arr[i]+ " ");
+		    }
+		    System.out.println();
+		    System.out.println("최대 값 : " + max);
+		    System.out.println("최소 값 : " + min);
+		}
+
+	public void practice13() {
+		
+		int[] arr = new int[10];
+	    for(int i = 0; i < arr.length; i++) {
+	        arr[i] = (int)(Math.random()*10+1);
+	        for(int j = 0; j < i; j++){
+	            if(arr[i] == arr[j]) {
+	                i--;	// 랜덤한수를 다시 만드는 코드
+	            }
+	        }
+	    }
+	    for(int i = 0; i < arr.length; i++) {
+	        System.out.print(arr[i] + " ");
+	    }
+	}
+
+	public void practice14() {
+
+		int[] arr = new int[6];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) (Math.random() * 45 + 1);
+			for (int j = 0; j < i; j++) {
+				if (arr[j] == arr[i]) {
+					i--;
+					break;
+				}
+			}
+
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+
+	}
+
+	public void practice15() {
+		
+		/*
+		 * 문자열을 입력 받아 문자열에 어떤 문자가 들어갔는지 배열에 저장하고
+          문자의 개수와 함께 출력하세요.
+		 * 
+		 * 
+		 * 
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("문자열 :");
+		String Str = sc.nextLine();
+		
+		char[]arr = new char[Str.length()];
+		int count =0;
+		for(int i=0; i<arr.length; i++) {
+			arr[i]=Str.charAt(i);
+			
+			boolean flag =true;
+			
+			for(int j=0; j<i; j++) {
+				if(arr[i]== arr[j]) {
+					flag = false;
+				}
+			}
+			if(flag) {
+				if(i==0) {
+					System.out.print(arr[i]);
+				}else {	
+				System.out.print( ","+ arr[i]);
+				
+			}
+			count++;
+			}
+		}
+		System.out.println("\n문자개수:"+ count);
+	}
+		
 	
+				
+		
+		
+
+	
+
+	public void practice16() {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("배열의 크기를 입력하세요 : ");
+		int num = sc.nextInt();
+
+		sc.nextLine();
+
+		String[] origin = new String[num];
+
+		// 배열 복사
+		String[] copy = origin;
+
+		for (int i = 0; i < origin.length; i++) {
+			System.out.printf("%d번째 문자열 : ", i + 1);
+			origin[i] = sc.nextLine();
+		}
+
+		while (true) {
+			System.out.print("더 값을 입력하시겠습니까? (Y/N)");
+			char ch = sc.nextLine().charAt(0);
+
+			if (ch == 'y' || ch == 'Y') { // 더입력하고 싶은 경우
+
+				System.out.print("더 입력하고싶은 개수 : ");
+				num = sc.nextInt();
+
+				sc.nextLine();
+
+				copy = Arrays.copyOf(origin, origin.length + num);
+
+				for (int i = origin.length; i < copy.length; i++) {
+					System.out.printf("%d번째 문자열", i + 1);
+					copy[i] = sc.nextLine();
+				}
+
+				// 젤 중요!!
+				origin = copy;
+
+			} else if (ch == 'n' || ch == 'N') { // 끝내고 싶은 경우
+
+				// 이따 출력
+				System.out.print("[");
+				for (int i = 0; i < copy.length; i++) {
+					if (i == copy.length - 1) {
+						System.out.printf("%s", copy[i]);
+					} else {
+						System.out.printf("%s, ", copy[i]);
+					}
+				}
+				System.out.print("]");
+				break;
+
+			} else { // 잘못입력한 경우
+				System.out.println("잘못입력하셨습니다. 다시 입력하세요");
+			}
+		}
+
+	}
+}
+    
